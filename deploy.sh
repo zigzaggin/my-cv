1a0451ebe2e1bb8979494b4c0a08b0a04f716a86
Ben Potter <benjamin.potter@live.com>
2019-02-03 12:55:20 -0700

    Deploy, cleanup, etc.

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:zigzaggin/my-cv.git master:gh-pages
+git push -f https://github.com/zigzaggin/my-cv.git master:gh-pages
 
 cd -
